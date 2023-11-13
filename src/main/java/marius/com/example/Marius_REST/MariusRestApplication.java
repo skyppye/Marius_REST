2304b14d3a213628d609d971356fcf441f1df834
skyppye2002 <skyppye2002@gmail.com>
2023-11-13 21:36:15 +0200

    forma initiala

---
+++ src/main/java/marius/com/example/Marius_REST/MariusRestApplication.java
@@ -0,0 +1,13 @@
+package marius.com.example.Marius_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariusRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariusRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marius/com/example/Marius_REST/MariusRestApplicationTests.java
@@ -0,0 +1,13 @@
+package marius.com.example.Marius_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariusRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
